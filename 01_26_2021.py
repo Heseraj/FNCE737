9f2c1f29be84ed6e51e3c8e7a5ce72f0f8995d11
Heseraj <Heseraj@hotmail.com>
2021-01-26 15:22:18 -0500

    Keep learning

---
+++ 01_26_2021.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 26 13:34:37 2021
+
+@author: HeSer
+"""
+#%% Load packages 
+# add as many comments as you can 
+
+import numpy as np 
+import pandas as pd 
+# x = 2 
+# print(x)
+
+# %%It is always a good idea to start simple 
+
+df = pd.read_csv("C:/Users/HeSer/OneDrive/Documents/Wharton/Wharton Spring 2021/FNCE737/FNCE737/Data/NKLA.csv")
+
+# ticker = "NKLA"
+
+# tt2= pd.read_csv("C:/Users/HeSer/OneDrive/Documents/Wharton/Wharton Spring 2021/FNCE737/FNCE737/Data/"+ ticker+".csv")
+
+#%% explore the data
+
+df.head(10)
+#%%
+df.tail(6)
+#%%
+df.column
+
+#%%
+
+df.describe()
+#%%
+df.dtypes()
+#%%
+df.info()
---
+++ Ch_3_Pandas.py
@@ -0,0 +1,6 @@
+#%% This is for practice in chapter 3, pandss
+
+import numpy as np
+import pandas as pd
+
+# talking about Vix, volatility index for s&p500
