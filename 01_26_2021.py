6633cd8f201225d548000647a41462a5abcd5eea
Heseraj <Heseraj@hotmail.com>
2021-01-28 17:36:18 -0500

    adding functions

--- 01_26_2021.py
+++ 01_26_2021.py
@@ -15,45 +15,58 @@ import pandas as pd
 # %%It is always a good idea to start simple 
 
 df = pd.read_csv("C:/Users/HeSer/OneDrive/Documents/Wharton/Wharton Spring 2021/FNCE737/FNCE737/Data/NKLA.csv")
-
+print(df.head(6))
 # ticker = "NKLA"
 
 # tt2= pd.read_csv("C:/Users/HeSer/OneDrive/Documents/Wharton/Wharton Spring 2021/FNCE737/FNCE737/Data/"+ ticker+".csv")
-
+#%%
+df.head(6)
 #%% explore the data
 
-df.head(10)
+# df.head(10)
 #%%
 
-df.tail(6)
+# df.tail(6)
 #%%
 
-df.columns()
+#df.columns()
 
 #%%
-df.describe()
+# df.describe()
 #%%
-df.dtypes()
+# df.dtypes()
 #%%
 df.info()
 #%%
 
+print(df.sort_values("users_holding"))
+# df.sort_values("user_holding", "timestamp")
 #%%
+df["datetime"]= pd.to_datetime(df["timestamp"])
+print(df.head(6))
+#%%
+df["date"] = df["datetime"].dt.date
+#%%
+df_daily = df.groupby("date").agg({"users_holding": "mean"})
+print(df_daily.head(6))
+#%%
+df_daily.index = pd.to_datetime(df_daily.index)
+#%%
+print(df_daily["2020"].head(20))
+# print("Hello")
+#%% get price and volume data from yahoo finance
+import yfinance as yf
+#%%
+tickers = "NKLA"
+start_date = df_daily.index.min()
+end_date = df_daily.index.max()
+freq = "id"
+p_df = yf.download(tickers, start=start_date, end=end_date)
 
 #%%
-
+print(p_df.head(6))
 #%%
-
-#%%
-
-#%%
-
-#%%
-
-#%%
-
-#%%
-
+p_df.head(20)
 #%%
 
 #%%
--- Page_Spider.py
+++ Page_Spider.py
@@ -1,10 +1,17 @@
 import os
 import argparse
+from utilities import url_utilities
 
 
 def main(database: str, url_list_file: str):
     print("We are going to work with ", database)  # the tutoring videos uses plus while here a comma responds
     print("We are going to scan ", url_list_file)  # the plus gives erorr
+    urls = url_utilities.load_urls_from_file(url_list_file)
+    for url in urls:
+        print("reading ", url)
+        page_content = url_utilities.load_page(url=url)
+        words = url_utilities.scrape_page(page_content=page_content)
+        big_word_list.extend(words)
 
 
 if __name__ == "__main__":
